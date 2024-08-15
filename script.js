873ff6878d5ac21120fcbc9148849b339b923e17
Cloudyky <ikhwanch131@gmail.com>
2024-08-15 22:42:24 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const switchTag = document.getElementsByClassName('switch')[0],
+        sliderTag = document.getElementsByClassName('slider'),
+        appImg = document.getElementsByClassName('app-img')[0],
+        bodyTag = document.querySelector('body');
+
+let light = 0,
+    dark = 1,
+    appApereance = light;
+
+function changeApereance() {
+    if(appApereance === light){
+        appApereance = dark;
+        console.log(appApereance);
+        switchTag.style.justifyContent = 'flex-end';
+        appImg.src = '/img/dark.png';
+        bodyTag.style.background = 'black';
+    }else{
+        appApereance = light;
+        console.log(appApereance);
+        switchTag.style.justifyContent = 'flex-start';
+        appImg.src = '/img/light.png';
+        bodyTag.style.background = 'white';
+    }
+}
+
+switchTag.addEventListener('click', changeApereance);
